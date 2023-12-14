73ea03097b05b5bc80d6f0b44d3a645c473154ee
2110200k <149522009+2110200k@users.noreply.github.com>
2023-12-14 23:47:13 +0500

    Add files via upload

---
+++ lab9/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'registration_screen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration Screen'),
+      ),
+      body: const RegistrationScreen(),
+    ),
+  ));
+}
